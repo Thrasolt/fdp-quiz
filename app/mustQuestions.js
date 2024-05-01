1206f2c28b75cf0e46e91e9f4e75f05be13f54de
timtressel <timres@gmx.net>
2024-05-01 13:24:29 +0200

    fix: adjust questions

--- app/mustQuestions.js
+++ app/mustQuestions.js
@@ -4,7 +4,7 @@ export const mustQuestions = [
         question: "Wer ist die Spitzenkandidatin der FDP zur Europawahl?",
         answer: "Marie-Agnes Strack-Zimmermann",
         options: [
-            "Marie-Agnes Strack-Zimmermann", "Heike Troles", "Ursual von der Leyen", "Annegret Kramp-Karrenbauer"
+            "Marie-Agnes Strack-Zimmermann", "Sabine Leutheusser-Schnarrenberger", "Ursual von der Leyen", "Annegret Kramp-Karrenbauer"
         ]
     },
     {
@@ -12,15 +12,15 @@ export const mustQuestions = [
         question: "Wer ist die Vorsitzende des Verteidigungsausschuss im Deutschen Bundestag?",
         answer: "Marie-Agnes Strack-Zimmermann",
         options: [
-            "Marie-Agnes Strack-Zimmermann", "Angela Merkel", "Michelle Obama", "Sahra Wagenknecht"
+            "Marie-Agnes Strack-Zimmermann", "Angela Merkel", "Heike Troles", "Sahra Wagenknecht"
         ]
     },
     {
         id: 21,
-        question: "Wer ist die bekannteste FDP Politikerin in Deutschland?",
+        question: "Wer ist einer der bekanntesten FDP Politikerin in Deutschland?",
         answer: "Marie-Agnes Strack-Zimmermann",
         options: [
-            "Marie-Agnes Strack-Zimmermann", "Franziska Brandmann", "Markus Schumacher", "Peter Cremerius"
+            "Marie-Agnes Strack-Zimmermann", "Ursula Kwasny", "Annalena Baerbock", "Michelle Obama"
         ]
     }
 ];
